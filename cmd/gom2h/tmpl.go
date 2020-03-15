221460df04c892efb68c9c9bb590ed2dd3f729e2
matsuyoshi <sfbgwm30@gmail.com>
2020-03-15 23:12:17 +0900

    refine

--- cmd/gom2h/tmpl.go
+++ cmd/gom2h/tmpl.go
@@ -3,28 +3,28 @@ package main
 const index = `<!DOCTYPE html>
 <html>
   <head>
- 		<meta charset="utf-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/styles/default.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js"></script>
     <script>hljs.initHighlightingOnLoad();</script>
     <style>
       {{ .Stylesheet }}
     </style>
-	  <style>
- 			body {
+    <style>
+      body {
         box-sizing: border-box;
         min-width: 200px;
         max-width: 980px;
         margin: 0 auto;
         padding: 45px;
       }
-      @media (max-width: 767px) {
-		    .markdown-body {
-			    padding: 15px;
-		    }
-	    }
-	  </style>
+    @media (max-width: 767px) {
+      .markdown-body {
+        padding: 15px;
+      }
+    }
+    </style>
   </head>
   <body>
     <article class="markdown-body">
