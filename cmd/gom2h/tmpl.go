184f91c90b141ac0257dd52236c88f877a2b0150
matsuyoshi <sfbgwm30@gmail.com>
2021-07-11 01:25:38 +0900

    Fix command testdata

--- cmd/gom2h/tmpl.go
+++ cmd/gom2h/tmpl.go
@@ -12,23 +12,24 @@ const index = `<!DOCTYPE html>
       {{ .Stylesheet }}
     </style>
     <style>
-      body {
+     body {
         box-sizing: border-box;
         min-width: 200px;
         max-width: 980px;
         margin: 0 auto;
         padding: 45px;
       }
-    @media (max-width: 767px) {
-      .markdown-body {
-        padding: 15px;
-      }
-    }
-    </style>
+     @media (max-width: 767px) {
+       .markdown-body {
+         padding: 15px;
+       }
+     }
+	  </style>
   </head>
   <body>
     <article class="markdown-body">
       {{ .Content }}
     </article>
   </body>
-</html>`
+</html>
+`
